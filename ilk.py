97b6203e08bf5cd22b518a6278fc2ed499aa7eac
Senann00 <seninmeherrem@gmail.com>
2025-04-11 10:40:03 +0400

    first commit

--- ilk.py
+++ ilk.py
@@ -72,3 +72,120 @@ val_float = float(val_str)
 print("10: Float:", val_float, type(val_float))
 val_int = int(val_float)
 print("10: Int:", val_int, type(val_int))
+
+
+
+# 1
+s = "Programming"
+print(len(s))       # Uzunluğu
+print(s[0])         # İlk simvolu
+
+# 2
+s1 = "Hello"
+s2 = "World"
+print(s1 + " " + s2)
+
+# 3
+text = "Python"
+print(text[-1])     # Son simvol
+
+# 4
+s = "Artificial"
+print(s.replace("Art", ""))  # "Art" hissəsini çıxarır
+
+# 5
+word = "Code"
+print(word[::-1])   # Tərsinə çevrilmiş versiya
+
+# 6
+s = "abcdefgh"
+print(s[::2])       # Hər ikinci simvol
+
+# 7
+text = "data"
+print(text.upper(), text.lower())
+
+# 8
+s = "Python-R-Java"
+print(s.split("-"))
+
+# 9
+ad = "Ayxan"
+yaş = 25
+print(f"{ad} {yaş} yaşındadır")
+
+# 10
+s = "salam-dunya"
+print(s.replace("-", " "))
+
+
+import pandas as pd
+
+# 1
+s1 = pd.Series([10, 20, 30, 40])
+
+# 2
+s1.index = ['w', 'x', 'y', 'z']
+
+# 3
+s2 = pd.Series({'p': 5, 'q': 10, 'r': 15})
+
+# 4
+print(s2['q'])
+
+# 5
+print(s1[s1 > 25])
+
+# 6
+print(s1[s1 > 20] / 10)
+
+# 7
+df1 = pd.DataFrame([[1, 2], [3, 4]])
+
+# 8
+df1.index = ['r1', 'r2']
+df1.columns = ['c1', 'c2']
+
+# 9
+df2 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+
+# 10
+print(df2[df2['A'] > 1])
+
+# 1
+rəqəmlər = [5, 10, 15, 20]
+
+# 2
+print(len(rəqəmlər))
+
+# 3
+rəqəmlər.append(25)
+
+# 4
+rəqəmlər.insert(2, 12)
+
+# 5
+birləşmiş = [1, 2, 3] + [4, 5, 6]
+
+# 6
+print(rəqəmlər[2:4])
+
+# 7
+rəqəmlər[0] = 50
+
+# 8
+print(19 in rəqəmlər)
+
+# 9
+lst = ["a", "b", "a", "c"]
+print(lst.count("a"))
+
+# 10
+lst2 = ["x", "y", "x", "z"]
+lst2 = [el for el in lst2 if el != "x"]
+
+# 11
+print(sorted([7, 2, 9, 1], reverse=True))
+
+# 12
+print([x for x in rəqəmlər if x > 10])
